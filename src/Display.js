2e11d2b7925a2d7d373bf098b6cc755a6ebe0a57
cphork <christina.phork@gmail.com>
2021-05-17 15:33:35 -0500

    pushing

---
+++ src/Display.js
@@ -0,0 +1,37 @@
+import React from "react";
+const Display = (props) => {
+  // destruct the dogs from props
+  const { dogs } = props
+
+  // Returns the JSX for when you have dogs
+  const loaded = () => (
+    <div style={{textAlign: "center"}}>
+      {dogs.map((dog) => (
+        <article key={dog._id}>
+          <img src={dog.img}/>
+          <h1>{dog.name}</h1>
+          <h3>{dog.age}</h3>
+
+{/* Edit button */}
+          <button onClick={() => {
+            props.selectDog(dog)
+            props.history.push("/edit")
+          }}>
+          Edit
+          </button>
+
+{/* Delete buttton */}
+          <button onClick ={() => {
+            props.deleteDog(dog)
+          }}>
+            Delete
+          </button>
+
+        </article>
+      ))}
+    </div>
+  )
+  const loading = () => <h1>Loading</h1>
+  return dogs.length > 0 ? loaded() : loading()
+};
+export default Display;
